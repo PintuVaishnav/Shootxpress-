ffd2afe073f138cd5f5f1feefef20b6b134655cf
PintuVaishnav <vaishnavyejju@gmail.com>
2025-12-18 16:23:26 +0530

    Updated the Gallery with new style

--- client/src/components/contact-section.tsx
+++ client/src/components/contact-section.tsx
@@ -221,7 +221,7 @@ export default function ContactSection() {
         <img
           src={leader.img}
           alt={leader.name}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain"
         />
       </div>
 
